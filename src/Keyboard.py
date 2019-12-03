34ef479eb0c0bc5845dfcec791b17baa8eae9e54
Taksh13 <ptaksh13@gmail.com>
2019-12-03 22:24:51 +0000

    Work from pi

--- src/Keyboard.py
+++ src/Keyboard.py
@@ -1,24 +1,46 @@
-from msvcrt import getch
-import msvcrt
+from gpiozero import LED
+from time import sleep
+import getch
 import time
 
+forwardled = LED(3)
+leftled = LED(4)
+reverseled = LED(14)
+rightled = LED(15)
+headlights = LED(17)
+taillights = LED(18)
 
-def main():
-    print("Please press a key to see its value")
-    while 1:
-        if msvcrt.kbhit():
-            key = ord(msvcrt.getch())
-            # print(key)
-            if key == 119:
-                print("Forward")
-            if key == 97:
-                print("Left")
-            if key == 115:
-                print("Reverse")
-            if key == 100:
-                print("Right")
-        time.sleep(0.001)
-
-
-if __name__ == "__main__":
-    main()
+print('Please press a key to see its value')
+while 1:
+    
+        headlights.on()
+        taillights.on()
+        key = getch.getch()
+        if key == 'w':
+            forwardled.on()
+            sleep(1)
+            forwardled.off()
+            print('Forward')
+        if key == 'a':
+            leftled.on()
+            sleep(1)
+            leftled.off()
+            print('Left')
+        if key == 's':
+            reverseled.on()
+            sleep(1)
+            reverseled.off()
+            print('Reverse')
+        if key == 'd':
+            rightled.on()
+            sleep(1)
+            rightled.off()
+            print('Right')
+        if key == 'h':
+            headlights.on()
+            sleep(1)
+            headlights.off()
+        if key == 't':
+            taillights.on()
+            sleep(1)
+            taillights.off()
