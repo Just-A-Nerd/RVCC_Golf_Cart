2627768b4004c5b34b08e02cbd9fb3aa3cbf8b38
kennycastro007 <kennycastro007@gmail.com>
2020-02-11 18:47:19 -0500

    Added voltage to led

--- KeyboardFinal.py
+++ KeyboardFinal.py
@@ -1,52 +1,61 @@
-from gpiozero import LED
+from gpiozero import PWMLED
 import pygame
 from pygame.locals import *
 
-import time
-from time import sleep
+forwardLed = PWMLED(3)
+forwardVoltage = 0
 
-forwardled = LED(3)
-leftled = LED(4)
-reverseled = LED(14)
-rightled = LED(15)
+leftLed = PWMLED(4)
+leftVoltage = 0
+
+reverseLed = PWMLED(14)
+reverseVoltage = 0
+
+rightLed = PWMLED(15)
+rightVoltage = 0
 
 screen = pygame.display.set_mode((400, 300))
 
 done = False
-    
-while not done: 
+
+while not done:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         print("Ending program")
         done = True
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_w:
-            forwardled.on()
+            forwardLed.on()
+
+            if forwardVoltage > 1:
+                forwardVoltage = 1
+
+            forwardLed.value = forwardVoltage
             print("w pressed")
         if event.key == pygame.K_a:
-            leftled.on()
+            leftLed.on()
             print("a pressed")
         if event.key == pygame.K_s:
-            reverseled.on()
+            forwardLed.off()
+            reverseLed.on()
             print("s pressed")
         if event.key == pygame.K_d:
-            rightled.on()
+            rightLed.on()
             print("d pressed")
 
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_w:
-            forwardled.off()
-            print("w  released")
+            forwardLed.off()
+            print("w released")
         if event.key == pygame.K_a:
-            leftled.off()
+            leftLed.off()
             print("a released")
         if event.key == pygame.K_s:
-            reverseled.off()
+            reverseLed.off()
             print("s released")
         if event.key == pygame.K_d:
-            rightled.off()
+            rightLed.off()
             print("d released")
-    
 
     pygame.display.flip()
     pygame.event.pump()
