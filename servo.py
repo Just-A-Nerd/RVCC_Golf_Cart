1b4db98569a8e3bdd795ee6a2b2d11eb44950b88
kennycastro007 <kennycastro007@gmail.com>
2020-02-19 16:06:53 -0500

    Added keyboard input to servo

--- servo.py
+++ servo.py
@@ -1,12 +1,26 @@
-from gpiozero import Servo
-from time import sleep
+from gpiozero import servo
+import pygame
+from pygame.locals import *
+
+
+screen = pygame.display.set_mode((400, 300))
 
 servo = Servo(17)
 
-while True:
-    servo.min()
-    sleep(1)
-    servo.mid()
-    sleep(1)
-    servo.max()
-    sleep(1)
+
+done = false
+while not done:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        done = true
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_w:
+            servo.value = 0
+        if event.key == pygame.K_s:
+            servo.value = 0
+    else:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            servo.max()
+        elif keys[pygame.K_s]:
+            servo.min()
